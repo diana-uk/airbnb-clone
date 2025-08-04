86be265b86b68964c80169317fb46c23021f80b0
MissDaizy <dianaukrainsky@gmail.com>
2025-08-04 15:31:47 +0300

    BNB-2 Enhance user registration flow with validation and duplicate email check

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -25,16 +25,18 @@ export class AuthController {
       // TODO 4: Add function to create this json for success
       res
         .status(201)
-        .json({ success: true, message: "User created successfully!" });
+        .json({
+          success: true,
+          data: createdUser,
+          message: "User created successfully!",
+        });
     } catch (error) {
       // TODO 2: Check if success false here is needed
       // TODO 3: Add function to create this json for error
-      res
-        .status(400)
-        .json({
-          success: false,
-          error: error instanceof Error ? error.message : "An error occurred",
-        });
+      res.status(400).json({
+        success: false,
+        error: error instanceof Error ? error.message : "An error occurred",
+      });
     }
   };
 }
